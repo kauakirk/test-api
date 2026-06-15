d07b44c82044b618226a337918cc69604d5ae4cf
Kirk Kaaua <kauakirk46@gmail.com>
2026-06-15 16:10:01 -0300

    refactor: update auth.py to use new UsuariosService class

--- utils/auth.py
+++ utils/auth.py
@@ -1,22 +1,24 @@
-from services.user_service import create_user, login
+from services.usuarios_service import UsuariosService
 from utils.payloads import new_user_payload
 
 
 def admin_token():
+    """Criar um token de admin para testes."""
     payload = new_user_payload(administrador="true")
-    create_response = create_user(payload)
+    create_response = UsuariosService.create(payload)
     assert create_response.status_code == 201
 
-    login_response = login({"email": payload["email"], "password": payload["password"]})
+    login_response = UsuariosService.login({"email": payload["email"], "password": payload["password"]})
     assert login_response.status_code == 200
     return login_response.json()["authorization"]
 
 
 def user_token():
+    """Criar um token de usuário comum para testes."""
     payload = new_user_payload(administrador="false")
-    create_response = create_user(payload)
+    create_response = UsuariosService.create(payload)
     assert create_response.status_code == 201
 
-    login_response = login({"email": payload["email"], "password": payload["password"]})
+    login_response = UsuariosService.login({"email": payload["email"], "password": payload["password"]})
     assert login_response.status_code == 200
     return login_response.json()["authorization"]
