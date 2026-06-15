2c04f6cf0de3b19efe502093bec0076dd14fce6e
Kirk Kaaua <kauakirk46@gmail.com>
2026-06-15 00:44:31 -0300

    feat: criando funcao de test_can_get_users

--- test_usuario.py
+++ test_usuario.py
@@ -10,6 +10,16 @@ def test_can_call_endpoint():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
+def test_can_get_users():
+    response = get_users()
+
+    body = response.json()
+    print(body)
+
+    assert response.status_code == 200
+    assert "quantidade" in body
+    assert "usuarios" in body
+    assert isinstance(body["usuarios"], list)
 
 def test_can_login():
     payload = login_payload()
@@ -26,8 +36,8 @@ def test_can_login():
 def post_login(payload):
     return requests.post(ENDPOINT + "/login", json=payload)
 
-def get_users(payload):
-    return requests.get(ENDPOINT + "/usuarios", json=payload)
+def get_users():
+    return requests.get(ENDPOINT + "/usuarios")
 
 def new_payload():
     unique_id = uuid.uuid4().hex
